dbb07a01cb3b6cca54d62d4a0afe54e5991adc45
arshavinbmgpersian-sudo <arshavinbmgpersian@gmail.com>
2026-06-10 07:04:36 +0330

    Nn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 export default {
-  async fetch(request) {
-    return new Response("Bot Online");
+  async fetch(request, env) {
+
+    if (request.method !== "POST") {
+      return new Response("Movie Bot Running");
+    }
+
+    const update = await request.json();
+
+    if (update.message) {
+
+      const chatId = update.message.chat.id;
+      const text = update.message.text || "";
+
+      await fetch(
+        `https://api.telegram.org/bot${env.BOT_TOKEN}/sendMessage`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            chat_id: chatId,
+            text: `پیام شما: ${text}`
+          })
+        }
+      );
+    }
+
+    return new Response("OK");
   }
 }
